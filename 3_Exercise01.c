fc8699ac1dbeca307d051bb812b1b18fa02520bf
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-02-22 23:39:32 +0530

    Add files via upload

---
+++ 3_Exercise01.c
@@ -0,0 +1,11 @@
+// write a C program to calculate area of a rectangle : 
+
+#include <stdio.h>
+
+int main()
+{
+    int length = 4 , breadth = 6 ;
+    int area = length * breadth ;
+    printf("The area of rectangle is %d ", area) ;  
+    return 0 ;
+}
