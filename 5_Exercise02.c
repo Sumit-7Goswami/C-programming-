0085f5b866eee2009f1ff6ed7406adf1754a0256
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-02-25 23:44:20 +0530

    Add files via upload

---
+++ 5_Exercise02.c
@@ -0,0 +1,16 @@
+// Write the area of the circle and modify the same program to calculate the volume of a cylinder given its radius and height 
+
+#include <stdio.h>
+
+int main()
+{
+    int radius = 3  ; 
+    float pi = 3.14 ;
+    printf("The area of this circle is %f \n", pi*radius*radius) ;
+
+    int height = 3 ;
+    printf("Volume of this cylinder is %d" , pi * radius * height) ;
+
+    return 0  ;
+
+}
