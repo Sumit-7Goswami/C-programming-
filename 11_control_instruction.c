7324fb7ba404349d58e3efdba2b41763ee4065d2
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-02 23:38:33 +0530

    Add files via upload

---
+++ 11_control_instruction.c
@@ -0,0 +1,11 @@
+// Control instruction
+// Detrmines the flow of control in a  program 
+// Four types of control instruction in C are :
+
+// 1) Sequence Control Instruction
+// 2) Decision Control Instruction
+// 3) Loop control Instruction
+// 4) Case control instruction
+
+
+
