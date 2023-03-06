72912add831c25303a3780f3e534c2538339d214
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-06 23:27:19 +0530

    Add files via upload

---
+++ 15_exercise_04.c
@@ -0,0 +1,17 @@
+// Explain step by step evaluation of 3*x/y-z+R   ; where x = 2   y = 3    z = 3   k = 1  ;
+
+
+#include<stdio.h>
+
+int main()
+{
+    int x = 2  , y = 3 , z = 3 , k = 1 ;
+    int result = 3 * x / y - z + k ;
+    // 6/3-3+1 ---> 2-3+1 --> 0
+    printf("The value of reult is %d" , result);
+    return 0 ;
+
+}
+
+
+//  Q         3.0+1 will be  a) integer   b)Floating point number 
