973efa265b26a266a611f2e3c453d6a74d7d2980
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-16 23:19:31 +0530

    Add files via upload

---
+++ 23_practice_01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10 ;
+    if(a = 11){
+    printf("I am 11 \n") ;
+    }
+
+    else{
+    printf("I am not 11 \n") ;
+    }
+
+    return 0 ;
+}
+
+// note   note****************************
+
+// on run above code you think the answer is I am not 11 , but the answer is I am 11  ,  because in if statement we have to make logical expression as a == 11 ,  then we got I am not 11 
