2b545a7ea52b466da176df6aa924ed37ebf743fe
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-22 23:34:31 +0530

    Add files via upload

---
+++ 28_loop_intro.c
@@ -0,0 +1,37 @@
+// sometimes we want our program to execute few set of instructions over and over again , for ex: printing 1 to 100 , first 100 even numbers etc.
+
+// Hence Loops make it easy for a programmer to tell computer taht a given set of instruction must be executed repeatedly .
+
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello") ; 
+    printf("World \n") ;
+
+    int a = 1 ;
+    printf("%d\n" , a); 
+    a++ ; 
+    printf("%d\n" , a); 
+    a++ ; 
+    printf("%d\n" , a); 
+    a++ ; 
+    printf("%d\n" , a); 
+    a++ ; 
+    printf("%d\n" , a); 
+    a++ ; 
+    printf("%d\n" , a); 
+    a++ ; 
+
+    // soo let take an if we have to write 1 to 600 ,, we cannot write like this thats why we use loop
+
+
+    return 0 ;
+}
+
+// Types of LOOP
+// 1) While loop
+// 2) do-while loop
+// 3) for loop
+
