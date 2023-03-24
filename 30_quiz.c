f9879b22d23457b5e9643c7db5302f0e96cf71c5
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-24 23:24:16 +0530

    Add files via upload

---
+++ 30_quiz.c
@@ -0,0 +1,22 @@
+//QUICK QUIZ // 
+//Write a program to print natural numbers 10 to 20 when initial loop counter i is initialised to 0 .
+
+#include <stdio.h>
+
+int main(){
+    int i = 0 ; 
+    while(i<=20){
+        if (i >= 10){
+            printf("The value of i is %d\n" , i ) ;
+        }
+        i++ ; 
+
+    }
+    return 0 ;
+}
+
+
+// The loop counter need not be int , it can be float as well 
+
+// i++    >phele print kro fir inccrement kro 
+//  ++1   phlee increment kre fir print kre
