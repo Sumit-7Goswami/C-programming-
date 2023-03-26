25a973aa3b50f6f32b87e5da32adca8a4c4db6cc
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-26 23:29:50 +0530

    Add files via upload

---
+++ 31_increment.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+
+// int main(){
+
+//     int i = 5 ; 
+//     printf("The value after i++ is %d\n" , i++) ;
+//     printf("The value of i is %d\n" , i) ; 
+
+
+//     return 0 ;
+// }
+
+//  =====---------==========-=-=-=-=-=-=-=-----===--======-----
+
+#include <stdio.h>
+
+int main(){
+
+    int i = 5 ; 
+
+    printf("The value after i++ is %d\n" , ++i) ;
+    
+    i++ ; // --> phele print fir increment
+    ++i ; // ---> phele increment fir print kre
+    printf("The value of i is %d\n" , i) ; 
+     
+
+    return 0 ;
+}
+
+
+//   +++ operator does not exist 
+//   i += 10   increment i  by 10
