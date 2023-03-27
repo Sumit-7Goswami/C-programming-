6d973c31fa85e420bdda5a869b08a3cf58ebb310
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-27 10:13:53 +0530

    Add files via upload

---
+++ 32_do_while-loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int i = 0 ;
+
+    do{
+        printf("The value of i is %d\n" , i ) ;
+        i++ ;
+    }while(i < 10) ;
+    return 0 ;
+
+    
+}
+
+/*do-while-loop works very similiar to while loop .
+
+while --> checks the condition & then executes the code
+do-while  ---> executes the code & then checks the condition*/
