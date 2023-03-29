8b7324a3315440e6c511e7e31de3b028d3ce458b
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-03-29 23:20:16 +0530

    Add files via upload

---
+++ 34_for_loop.c
@@ -0,0 +1,21 @@
+// syntax of for loop look like this ;
+/*  for(initialize  ;  test  ; increment or decrement )
+    {
+        //code 
+        ..code
+        code 
+    }
+    
+    */
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    for (int a=0 ; a<10 ; a++){
+        printf("The value of a is %d \n" , a) ;
+    }
+    return 0 ;
+}
