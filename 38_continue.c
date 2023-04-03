13ba98726af4b4e3c8d9a52cfdc6d2d7faf94ef6
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-04-03 23:40:21 +0530

    Add files via upload

---
+++ 38_continue.c
@@ -0,0 +1,19 @@
+// The continue statement is used to immediately move to the next iteration of the loop 
+
+
+#include <stdio.h>
+
+int main()
+{
+    int skip = 5 , i = 0 ;
+    while(i<10){
+        i++ ;
+        if (i != skip){
+            continue;
+        }
+        else{
+            printf("%d\n" , i) ;
+        }
+    }
+    return 0 ;
+}
