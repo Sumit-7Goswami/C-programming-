1bbb81d0b03802a25a3d5db550b175044285d430
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-04-04 23:29:33 +0530

    Add files via upload

---
+++ 40_practice02.c
@@ -0,0 +1,19 @@
+// write a program to print multiplication table of 10 in reverse order 
+
+/* we will do this any of the loop method , now we do with while loop*/
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 10; i ; i--)
+    {
+        printf("10 x %d = %d\n" , i , 10*i ) ; 
+    }
+    
+    return 0 ;
+}
+
+
+
+// A do while loop is executed at least once . 
