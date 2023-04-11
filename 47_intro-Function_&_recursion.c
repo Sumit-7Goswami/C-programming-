e662973e96d775e1bd6596ef2650fd89610a2fcd
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-04-11 23:15:48 +0530

    Add files via upload

---
+++ 47_intro-Function_&_recursion.c
@@ -0,0 +1,23 @@
+//Sometimes our program gets bgger in size and it is not possible for a programmer to track which piece of code is doing what .
+////////////function is a way to break our code into chunks so that  is possible for a programmer to reuse them .
+
+// what is a function ?
+// A function is a block of code which performs a particular task , A function can be reused by the programmer in a given program any number of times .
+
+/* Examples and syntax of a Function 
+
+#include<stdio.h>
+void display();    ---->function prototype
+
+int main(){
+    int  a ;
+    display();  ---------> Function call 
+    return 0 ; 
+}
+
+void display(){
+    printf("Hi I am display") ;
+}
+
+
+*/
