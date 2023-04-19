eb1b00811533e98ea297ffa5e70ee59202acf527
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-04-19 23:39:35 +0530

    Add files via upload

---
+++ 56_exercise-03.c
@@ -0,0 +1,30 @@
+// write a program using recursion to calculate the nth element of fibonacci series 
+
+
+#include <stdio.h>
+
+int main(){
+
+
+    return 0 ; 
+}
+
+
+
+
+//*** *** *** what will this line print  ----->   printf("%d %d %d" , a , ++a , a++)
+
+
+#include <stdio.h>
+
+int main(){
+    int a = 3 ; 
+
+    printf("%d %d %d" , a , ++a , a++) ; 
+
+
+
+    return 0 ; 
+}
+
+
