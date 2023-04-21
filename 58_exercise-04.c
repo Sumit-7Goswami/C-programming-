9d847404de40cacf32c873b97476276a5de2a291
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-04-21 22:36:50 +0530

    Add files via upload

---
+++ 58_exercise-04.c
@@ -0,0 +1,21 @@
+// write a program using function to print the following pattern(first n lines) 
+
+/*
+
+*
+
+*  *  *
+
+*  *  *  *  *
+
+
+...                                                                        */
+
+
+#include <stdio.h>
+
+int main(){
+
+
+    return 0 ; 
+}
