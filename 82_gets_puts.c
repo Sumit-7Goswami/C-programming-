7ebff889d28af310bc185318ba7c73cde6fbdec4
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-05-13 23:48:59 +0530

    Add files via upload

---
+++ 82_gets_puts.c
@@ -0,0 +1,42 @@
+
+/*
+gets() is a function which can be used to receive a multi-word string
+
+example --
+                char st[30] ;
+                gets(st) ;      ===> The entered string is stired in St .
+
+Multiple gets() calls wul be needed for multiple strings
+
+Likewise , puts() can be used to output a string .
+
+puts(st) ;    ---> prints the string places the cursor on the next line
+
+
+*/
+
+// #include <stdio.h>
+// int main(){
+
+//     char s[34] ;
+//     printf("enter the your name") ;
+//     scanf("%s" , &s) ;
+//     printf("your name is %s" , s) ;
+
+//     return 0 ;
+// }
+// in above code , when we write the name and surname , then our output gets as a name only , and surname will be missed .
+// so we use gets() to get multi-word string
+
+
+#include <stdio.h>
+int main()
+{
+
+    char s[34];
+    printf("enter your name");
+    gets(s);
+    puts(s) ; 
+    // printf("your name is %s", s);
+    return 0;
+}
